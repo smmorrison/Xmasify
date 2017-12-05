dd59112c89a0e8b94f82f052943683b1707e6145
smmorrison <samantha.morrison@hotmail.se>
2017-12-05 11:59:22 +0100

    first commit

---
+++ app/src/main/java/com/example/samanthamorrison/xmasify/SpecificDayActivity.kt
@@ -0,0 +1,13 @@
+package com.example.samanthamorrison.xmasify
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SpecificDayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_specific_day)
+    }
+}
